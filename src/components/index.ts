b0abfaebda28541ff98314e2440d319ca2c9e05a
Aaron Teo <aaron.teo@riv-alumni.com>
2020-11-09 22:59:28 +0800

    Added loading screen

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export * from './Container';
 export * from './PageHeader';
 export * from './SectionHeader';
 export * from './StaticCard';
+
+export * from './LoadingStatus';
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { PageHeader } from '../components';
-import { FirebaseService } from '../services';
+import { AppState, FirebaseService } from '../services';
+import { PageHeader, LoadingStatus } from '../components';
 
 const Login: React.FC = () => {
   const firebase = FirebaseService.getInstance();
+  const authStatus = useSelector((state: AppState) => state.user_status);
+
+  if (authStatus.loading) return <LoadingStatus />;
 
   return (
     <section>
