3f04126f9050ccb0af1bdcca6ae70ad8d22faf8d
Aaron Teo <aaron.teo@riv-alumni.com>
2021-01-02 19:21:09 +0800

    Moved utility functions back to TableComponent

--- src/pages/Participations/TableComponent.tsx
+++ src/pages/Participations/TableComponent.tsx
@@ -1,8 +1,8 @@
 import React, { memo, useEffect, useState } from 'react';
 
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable, BehaviorSubject, of, combineLatest } from 'rxjs';
 import {
-  skip,
+  map,
   switchMap,
   debounceTime,
   distinctUntilChanged,
@@ -12,12 +12,69 @@ import { firestore } from 'firebase/app';
 import { collectionData } from 'rxfire/firestore';
 
 import { Member, Participation } from '../../models';
-import { FirebaseServiceUtilities } from '../../services/NewFirebaseService';
+import { FirebaseService } from '../../services';
+
+/**
+ * Configurable settings for Firestore queries.
+ */
+const queryLimit = 15;
+const maxVIAHours = 9999;
+const maxEventCode = 99999999;
+
+const firebase = FirebaseService.getInstance();
 
 const onSearch$ = new BehaviorSubject<number>(0);
 
 const TableComponent: React.FC = memo(() => {
   const [data, setData] = useState<(Member & Participation)[]>([]);
+  // const [lastDoc, setLastDoc] = useState<Participation | null>(null);
+
+  /**
+   * Queries the participation collection depending on the search query.
+   *
+   * @param eventCode Unique event code
+   *
+   * @description
+   * If there is an event code (>= 0), the search-specific query will be
+   * used in place instead of the default query against the entire collection.
+   */
+  const getParticipations = (
+    eventCode: number
+  ): Observable<Participation[]> => {
+    if (eventCode <= 0) {
+      const query = firestore()
+        .collection('participations')
+        .orderBy('VIA Hours', 'desc')
+        .orderBy('Event Code', 'desc')
+        .startAfter(maxEventCode)
+        .limit(queryLimit);
+
+      return collectionData<Participation>(query);
+    }
+
+    const query = firestore()
+      .collection('participations')
+      .where('Event Code', '==', eventCode)
+      .orderBy('VIA Hours', 'desc')
+      .startAfter(maxVIAHours)
+      .limit(queryLimit);
+
+    return collectionData<Participation>(query);
+  };
+
+  const getFullParticipation = (
+    participations: Participation[]
+  ): Observable<(Member & Participation)[]> => {
+    if (participations.length === 0) return of([]);
+
+    return combineLatest(
+      participations.map((participation) =>
+        firebase
+          .getMemberDoc(participation['Membership ID'])
+          .pipe(map((member) => ({ ...member, ...participation })))
+      )
+    );
+  };
 
   useEffect(() => {
     /**
@@ -25,57 +82,29 @@ const TableComponent: React.FC = memo(() => {
      * https://stackoverflow.com/questions/48234431/query-data-relationship-on-firebase-realtime-database-with-angularfire2
      */
 
-    /**
-     * Queries the participation collection depending on the search query.
-     *
-     * @param eventCode Unique event code
-     *
-     * @description
-     * If there is an event code (>= 0), the search-specific query will be
-     * used in place instead of the default query against the entire collection.
-     */
-    const getParticipations = (
-      eventCode: number
-    ): Observable<Participation[]> => {
-      if (eventCode <= 0) {
-        const query = firestore()
-          .collection('participations')
-          .orderBy('VIA Hours', 'desc')
-          .orderBy('Event Code', 'desc')
-          .limit(10);
-
-        return collectionData<Participation>(query);
-      }
-
-      const query = firestore()
-        .collection('participations')
-        .where('Event Code', '==', eventCode)
-        .orderBy('VIA Hours', 'desc')
-        .limit(10);
-
-      return collectionData<Participation>(query);
-    };
-
     const sub = onSearch$
       .pipe(
         debounceTime(500),
         distinctUntilChanged(),
         switchMap(getParticipations),
-        switchMap(FirebaseServiceUtilities.mergeFullParticipationDetails),
-        skip(1)
+        switchMap(getFullParticipation)
       )
       .subscribe(setData);
 
     return () => sub.unsubscribe();
-  }, [setData]);
+  }, []);
 
   return (
-    <div className='table-responsive'>
+    <div
+      className='table-responsive'
+      style={{
+        maxHeight: '70vh',
+      }}>
       <table className='table table-hover table-borderless mb-0'>
         <thead>
           <tr>
             <th>No.</th>
-            <th>Membership ID</th>
+            <th>Full Name</th>
             <th>Event Code</th>
             <th>Role</th>
             <th>VIA Hours</th>
@@ -102,7 +131,7 @@ const TableComponent: React.FC = memo(() => {
           {data.map((prt, i) => (
             <tr key={prt['Membership ID'] + prt['Event Code']}>
               <td>{i + 1}</td>
-              <td>{prt['Full Name']}</td>
+              <td className='text-truncate'>{prt['Full Name']}</td>
               <td>{prt['Event Code']}</td>
               <td>{prt['Role']}</td>
               <td>{prt['VIA Hours']}</td>
@@ -110,7 +139,7 @@ const TableComponent: React.FC = memo(() => {
           ))}
         </tbody>
 
-        <caption>Results limited to 10 only.</caption>
+        <caption>Results limited to {queryLimit} only.</caption>
       </table>
     </div>
   );
