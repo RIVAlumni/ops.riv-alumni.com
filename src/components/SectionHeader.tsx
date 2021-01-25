1ad6f57f4e4b666e99b9139564f2706654d6067e
Aaron Teo <aaron.teo@riv-alumni.com>
2021-01-25 13:46:27 +0800

    Fixed alignment issues

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -14,11 +14,11 @@ const SectionHeader: React.FC<ISectionHeaderProps> = ({
   return (
     <div className='row'>
       <div className='col-sm-12 col-md-9 col-lg-10'>
-        <h3 className='mb-0 mb-md-2 mb-lg-2 font-weight-light'>{children}</h3>
+        <h3 className='font-weight-light'>{children}</h3>
       </div>
 
       {linkTo && linkText && (
-        <div className='col-sm-12 col-md-3 col-lg-2 d-flex align-items-center justify-content-end mb-2'>
+        <div className='mb-2 col-sm-12 col-md-3 col-lg-2 d-flex align-items-center justify-content-start justify-content-md-end justify-content-lg-end'>
           <Link className='text-dark' to={linkTo}>
             {linkText}
             <i className='ml-2 fas fa-chevron-right' />
