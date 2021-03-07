90e3ff5105e1c072baf4b282c2f61b18b1987362
Aaron Teo <aaron.teo@riv-alumni.com>
2021-03-07 13:55:31 +0800

    Updated component types for IntelliSense

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
+import React, { HTMLProps } from 'react';
 import { useField, FieldAttributes } from 'formik';
 
-type InputFieldProps = { label: string } & FieldAttributes<any>;
+type InputFieldProps = { label: string } & FieldAttributes<
+  HTMLProps<HTMLInputElement>
+>;
 
-const InputField: React.FC<
-  React.HTMLProps<HTMLInputElement> & InputFieldProps
-> = ({ label, ...props }) => {
-  const [field, meta] = useField<any>(props);
+const InputField: React.FC<InputFieldProps> = ({ label, ...props }) => {
+  const [field, meta] = useField<string>(props.name);
 
   const errorText = meta.error && meta.touched ? meta.error : '';
 
   return (
-    <div className='row py-2'>
-      <div className='col-sm-12 col-md-4 col-lg-4'>
-        <label
-          htmlFor={props.id || props.name}
-          className={`font-weight-bold ${!!errorText ? 'text-danger' : null}`}>
-          {label}
-        </label>
-      </div>
+    <div className='form-group'>
+      <div className='row'>
+        <div className='col-sm-12 col-md-4 col-lg-4 align-self-center'>
+          <label className='m-0' htmlFor={props.id || props.name}>
+            {label}
+          </label>
+        </div>
 
-      <div className='col-sm-12 col-md-8 col-lg-8'>
-        <input
-          {...field}
-          {...props}
-          className='p-2 px-3 w-100'
-          value={field.value || ''}
-          placeholder={`No ${props.id || props.name} set`}
-          style={{
-            border: 'none',
-            borderRadius: '5px',
-            backgroundColor: '#a4b0be',
-          }}
-        />
+        <div className='col-sm-12 col-md-8 col-lg-8'>
+          <input
+            {...field}
+            {...props}
+            value={field.value || ''}
+            className={`p-2 px-3 w-100 form-control ${
+              !!errorText ? 'is-invalid' : ''
+            }`}
+            placeholder={`No ${props.id || props.name} set`}
+            style={{
+              border: 'none',
+              borderRadius: '5px',
+              backgroundColor: '#a4b0be',
+            }}
+          />
+
+          <small className='form-text invalid-feedback'>{errorText}</small>
+        </div>
       </div>
     </div>
   );
--- src/components/SelectField.tsx
+++ src/components/SelectField.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
-import { FieldAttributes, useField } from 'formik';
+import React, { HTMLProps } from 'react';
+import { useField, FieldAttributes } from 'formik';
 
-type SelectFieldProps = { label: string } & FieldAttributes<any>;
+type SelectFieldProps = { label: string } & FieldAttributes<
+  HTMLProps<HTMLSelectElement>
+>;
 
-const SelectField: React.FC<
-  React.HTMLProps<HTMLSelectElement> & SelectFieldProps
-> = ({ label, children, ...props }) => {
-  const [field, meta] = useField<any>(props);
+const SelectField: React.FC<SelectFieldProps> = ({
+  label,
+  children,
+  ...props
+}) => {
+  const [field, meta] = useField(props.name);
 
-  const error = meta.error && meta.touched;
+  const errorText = meta.error && meta.touched ? meta.error : '';
 
   return (
-    <div className='row py-2'>
-      <div className='col-sm-12 col-md-4 col-lg-4'>
-        <label
-          htmlFor={props.id || props.name}
-          className={`font-weight-bold ${!!error ? 'text-danger' : null}`}>
-          {label}
-        </label>
-      </div>
+    <div className='form-group'>
+      <div className='row'>
+        <div className='col-sm-12 col-md-4 col-lg-4 align-self-center'>
+          <label htmlFor={props.id || props.name}>{label}</label>
+        </div>
 
-      <div className='col-sm-12 col-md-8 col-lg-8'>
-        <select
-          {...field}
-          {...props}
-          className='p-2 px-3 w-100'
-          style={{
-            appearance: 'none',
-            border: 'none',
-            borderRadius: '5px',
-            backgroundColor: '#a4b0be',
-          }}>
-          {children}
-        </select>
+        <div className='col-sm-12 col-md-8 col-lg-8'>
+          <select
+            {...field}
+            {...props}
+            className={`p-2 px-3 w-100 form-control ${
+              !!errorText ? 'is-invalid' : ''
+            }`}
+            style={{
+              border: 'none',
+              appearance: 'none',
+              borderRadius: '5px',
+              backgroundColor: '#a4b0be',
+            }}>
+            {children}
+          </select>
+        </div>
       </div>
     </div>
   );
