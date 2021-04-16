2cd5f8bdeffe3d8006dcc7cf2c2c4c9c8b628fc3
Aaron Teo <aaron.teo@riv-alumni.com>
2021-04-17 01:26:53 +0800

    Updated AddEvent page

--- src/pages/AddEvent/index.tsx
+++ src/pages/AddEvent/index.tsx
@@ -5,6 +5,9 @@ import { Form, Formik } from 'formik';
 import { FORM_SCHEMA_EVENT } from '../../constants';
 import { PageHeader, SectionHeader } from '../../components';
 
+import { Button } from '../../ui/Button';
+import { ButtonLink } from '../../ui/ButtonLink';
+
 const AddEvent: React.FC = () => {
   return (
     <section>
@@ -16,114 +19,106 @@ const AddEvent: React.FC = () => {
         initialValues={{}}
         validationSchema={FORM_SCHEMA_EVENT}
         onSubmit={() => {}}>
-        <Form className='grid-container'>
-          <div className='grid-span-8'>
-            <input
-              type='number'
-              className='w-100'
-              placeholder='Event Code'
-              min={0}
-              max={99999999}
-              minLength={8}
-              maxLength={8}
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}
-            />
-          </div>
-
-          <div className='grid-span-4'>
-            <select
-              className='w-100 h-100 custom-select custom-select-sm'
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}>
-              <option value='false' selected>
-                Unofficial Event
-              </option>
-              <option value='true'>Official Event</option>
-            </select>
-          </div>
-
-          <div className='grid-span-12'>
-            <input
-              type='text'
-              className='w-100'
-              placeholder='Event Name'
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}
-            />
-          </div>
-
-          <div className='grid-span-6'>
-            <input
-              type='text'
-              className='w-100'
-              placeholder='Overall In-Charge'
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}
-            />
-          </div>
-
-          <div className='grid-span-6'>
-            <input
-              type='text'
-              className='w-100'
-              placeholder='Assistant In-Charge'
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}
-            />
-          </div>
-
-          <div className='grid-span-12'>
-            <input
-              type='text'
-              className='w-100'
-              placeholder='Google Drive URL'
-              style={{
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '8px',
-                backgroundColor: '#a4b0be',
-              }}
-            />
-          </div>
-
-          <div className='grid-span-12 row justify-content-start'>
-            <div className='col-4'>
-              <button
-                className='w-100 btn btn-danger'
+        <Form>
+          <div className='grid-container'>
+            <div className='grid-span-8'>
+              <input
+                type='number'
+                className='w-100'
+                placeholder='Event Code'
+                min={0}
+                max={99999999}
+                minLength={8}
+                maxLength={8}
                 style={{
-                  padding: '10px 40px',
+                  padding: '10px 20px',
+                  border: 'none',
                   borderRadius: '8px',
-                }}>
-                Add Event
-              </button>
+                  backgroundColor: '#a4b0be',
+                }}
+              />
             </div>
 
-            <div className='col-4 align-self-center'>
-              <a href='/' className='text-dark'>
-                Cancel
-              </a>
+            <div className='grid-span-4'>
+              <select
+                className='w-100 h-100 custom-select custom-select-sm'
+                style={{
+                  padding: '10px 20px',
+                  border: 'none',
+                  borderRadius: '8px',
+                  backgroundColor: '#a4b0be',
+                }}>
+                <option value='false' selected>
+                  Unofficial Event
+                </option>
+                <option value='true'>Official Event</option>
+              </select>
             </div>
+
+            <div className='grid-span-12'>
+              <input
+                type='text'
+                className='w-100'
+                placeholder='Event Name'
+                style={{
+                  padding: '10px 20px',
+                  border: 'none',
+                  borderRadius: '8px',
+                  backgroundColor: '#a4b0be',
+                }}
+              />
+            </div>
+
+            <div className='grid-span-6'>
+              <input
+                type='text'
+                className='w-100'
+                placeholder='Overall In-Charge'
+                style={{
+                  padding: '10px 20px',
+                  border: 'none',
+                  borderRadius: '8px',
+                  backgroundColor: '#a4b0be',
+                }}
+              />
+            </div>
+
+            <div className='grid-span-6'>
+              <input
+                type='text'
+                className='w-100'
+                placeholder='Assistant In-Charge'
+                style={{
+                  padding: '10px 20px',
+                  border: 'none',
+                  borderRadius: '8px',
+                  backgroundColor: '#a4b0be',
+                }}
+              />
+            </div>
+
+            <div className='grid-span-12'>
+              <input
+                type='text'
+                className='w-100'
+                placeholder='Google Drive URL'
+                style={{
+                  padding: '10px 20px',
+                  border: 'none',
+                  borderRadius: '8px',
+                  backgroundColor: '#a4b0be',
+                }}
+              />
+            </div>
+          </div>
+
+          <div className='btn-group mt-4'>
+            <Button color='danger' className='mr-3'>
+              Add Event
+            </Button>
+            <ButtonLink to='/manage/events' className='ml-3'>
+              Cancel
+            </ButtonLink>
           </div>
         </Form>
       </Formik>
