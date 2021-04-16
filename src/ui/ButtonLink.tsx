1168059da7fad880356c07e401a96c6a3ebb8075
Aaron Teo <aaron.teo@riv-alumni.com>
2021-04-17 01:15:00 +0800

    Added new ButtonLink UI element

---
+++ src/ui/ButtonLink.tsx
@@ -0,0 +1,29 @@
+import React, { RefAttributes } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+type ButtonLinkProps = LinkProps &
+  RefAttributes<HTMLAnchorElement> & {
+    to: string;
+  };
+
+const ButtonLinkElement: React.FC<ButtonLinkProps> = ({
+  to,
+  children,
+  className,
+  ...props
+}) => (
+  <Link
+    to={to}
+    className={`d-flex align-items-center justify-content-center text-dark ${className}`}
+    {...props}>
+    {children}
+  </Link>
+);
+
+export const ButtonLink = styled(ButtonLinkElement)`
+  font-size: 1rem;
+  line-height: 1.5rem;
+  text-decoration: underline;
+`;
