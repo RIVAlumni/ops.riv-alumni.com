00b194fc4c8a217fb2b9299d5020a26f1c6f408b
Aaron Teo <aaron.teo@riv-alumni.com>
2021-05-14 04:10:51 +0800

    Updated form component to match forwardRef

--- src/components/form/FormSelect.tsx
+++ src/components/form/FormSelect.tsx
@@ -1,5 +1,5 @@
 import { useField, FieldAttributes } from 'formik';
-import { DetailedHTMLProps, SelectHTMLAttributes } from 'react';
+import { forwardRef, DetailedHTMLProps, SelectHTMLAttributes } from 'react';
 
 import { NativeSelect } from '../../ui';
 import { SelectOptions } from '../../constants';
@@ -8,34 +8,33 @@ type FormSelectProps = FieldAttributes<
   DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 > & { label?: string; options: SelectOptions<any>[] };
 
-const FormSelect: React.FC<FormSelectProps> = ({
-  label,
-  options,
-  ...props
-}) => {
-  const [field, meta, { setValue }] = useField(props.name);
-  const errorText = meta.error && meta.touched ? meta.error : '';
+const FormSelect = forwardRef<HTMLSelectElement, FormSelectProps>(
+  ({ label, options, ...props }, ref) => {
+    const [field, meta, { setValue }] = useField(props.name);
+    const errorText = meta.error && meta.touched ? meta.error : '';
 
-  return (
-    <div className='w-100 h-100'>
-      <label htmlFor={field.name} className='mb-1 w-100'>
-        {label || props.placeholder || props.name}
-      </label>
+    return (
+      <div className='w-100 h-100'>
+        <label htmlFor={field.name} className='mb-1 w-100'>
+          {label || props.placeholder || props.name}
+        </label>
 
-      <NativeSelect
-        {...field}
-        {...props}
-        id={field.name}
-        options={options}
-        onChange={(e) => {
-          const number: number = Number(e.currentTarget.value);
-          return setValue(isNaN(number) ? e.currentTarget.value : number);
-        }}
-      />
+        <NativeSelect
+          {...field}
+          {...props}
+          ref={ref}
+          id={field.name}
+          options={options}
+          onChange={(e) => {
+            const number: number = Number(e.currentTarget.value);
+            return setValue(isNaN(number) ? e.currentTarget.value : number);
+          }}
+        />
 
-      <div className='mt-1 text-danger'>{errorText}</div>
-    </div>
-  );
-};
+        <div className='mt-1 text-danger'>{errorText}</div>
+      </div>
+    );
+  }
+);
 
 export { FormSelect };
