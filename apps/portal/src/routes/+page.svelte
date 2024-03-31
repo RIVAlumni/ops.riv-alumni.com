6c491276c8435580af3b2a54a6f6f8f341ef839e
Aaron Teo <taronaeo@gmail.com>
2024-03-31 16:49:02 +0800

    feat(portal): add shadcn-svelte

--- apps/portal/src/routes/+page.svelte
+++ apps/portal/src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h1 class="text-4xl font-bold">You've reached RIVAlumni Portal</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+</script>
+
+<Card.Root class="w-full max-w-sm">
+  <Card.Header>
+    <Card.Title class="text-2xl">Login</Card.Title>
+    <Card.Description>Enter your email below to login to your account.</Card.Description>
+  </Card.Header>
+  <Card.Content class="grid gap-4">
+    <div class="grid gap-2">
+      <Label for="email">Email</Label>
+      <Input id="email" type="email" placeholder="m@example.com" required />
+    </div>
+    <div class="grid gap-2">
+      <Label for="password">Password</Label>
+      <Input id="password" type="password" required />
+    </div>
+  </Card.Content>
+  <Card.Footer>
+    <Button class="w-full">Sign in</Button>
+  </Card.Footer>
+</Card.Root>
