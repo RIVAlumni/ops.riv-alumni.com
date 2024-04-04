e0000648bf844898708622b5b8933dc15fdf4d96
Aaron Teo <taronaeo@gmail.com>
2024-04-04 19:44:35 +0800

    feat(portal): finish scaffold of overview page

---
+++ portal/src/routes/(app)/overview/(components)/event-card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let eventName: string = 'Unspecified';
+  export let eventLocation: string = 'Undetermined';
+  export let backgroundImage: string;
+</script>
+
+<div class="flex flex-col gap-4">
+  <div
+    class="w-full h-auto rounded-md
+            bg-cover bg-center aspect-video
+            transition-all hover:scale-105"
+    style="background-image: url({backgroundImage});">
+  </div>
+
+  <div class="text-sm space-y-1">
+    <h3 class="leading-none font-medium">{eventName}</h3>
+    <p class="text-xs text-muted-foreground">
+      <i
+        class="mr-1 text-primary
+                fa-sharp fa-solid fa-location-dot"></i>
+      {eventLocation}
+    </p>
+  </div>
+</div>
---
+++ portal/src/routes/(app)/overview/(components)/profile-card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card';
+  import * as Avatar from '$lib/components/ui/avatar';
+
+  import { Button } from '$lib/components/ui/button';
+</script>
+
+<Card.Root
+  class="sm:col-span-2
+          bg-cover bg-right-top bg-no-repeat bg-img-profile-card">
+  <Card.Header class="pb-4">
+    <Avatar.Root class="w-28 h-28 my-5">
+      <Avatar.Image
+        src=""
+        alt="Profile Picture" />
+      <Avatar.Fallback class="flex flex-col justify-center">
+        <i class="fa-2xl fa-regular fa-user"></i>
+      </Avatar.Fallback>
+    </Avatar.Root>
+
+    <Card.Title class="text-4xl">Sign in to your account</Card.Title>
+    <Card.Description>
+      Get the full experience of being an alumni by signing into your account
+      now
+    </Card.Description>
+  </Card.Header>
+  <Card.Footer>
+    <Button href="/auth/continue">
+      Continue
+      <i class="ml-2 fa-sharp fa-solid fa-arrow-right"></i>
+    </Button>
+  </Card.Footer>
+</Card.Root>
