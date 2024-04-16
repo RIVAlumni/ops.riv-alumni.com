89e32595a8f4ba81056d045216c8ea02e12e75fe
Aaron Teo <taronaeo@gmail.com>
2024-04-17 01:45:11 +0800

    fix(portal): erroneous types for fsuser

--- packages/shared/src/models/FSUser.ts
+++ packages/shared/src/models/FSUser.ts
@@ -1,16 +1,16 @@
-import type { FSMetadata } from '$lib/models';
+import type { FSMetadata } from './FSMetadata';
 
 /**
  * Firebase Auth user
  */
 interface FSUser extends FSMetadata {
   readonly uid: string;
-  readonly email: string;
-  readonly email_verified: string;
-  readonly display_name: string;
-  readonly photo_url: string;
-  readonly is_onboarded: string;
-  readonly is_suspended: string;
+  readonly email: string | null;
+  readonly email_verified: boolean;
+  readonly display_name: string | null;
+  readonly photo_url: string | null;
+  readonly is_onboarded: boolean;
+  readonly is_suspended: boolean;
 }
 
 export type { FSUser };
